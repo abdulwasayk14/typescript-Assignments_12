8e46b36eccfa21cb2233b8511d1dcc5ce2324ee2
Abdul wasay khan <156195187+abdulwasayk14@users.noreply.github.com>
2024-02-27 09:28:07 +0500

    Add files via upload

---
+++ Name.js
@@ -0,0 +1,6 @@
+"use strict";
+let names = ['Talha', 'Arif', 'Asif', 'Taha'];
+const message = 'Welcome to Typescript:';
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
---
+++ Name.ts
@@ -0,0 +1,11 @@
+
+
+let names :string[] = ['Talha','Arif','Asif','Taha'] ;
+
+const message :string = 'Welcome to Typescript:'
+
+for(let i=0; i<names.length; i++){
+
+    console.log(names[i]) ;
+
+}
